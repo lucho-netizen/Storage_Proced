761da3b934822358e55b1f81cbec1801c7edb850
Paco <114266465+lucho-netizen@users.noreply.github.com>
2023-08-26 14:36:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+// Configuración de la conexión a la base de datos
+const connection = {
+  connectionLimit: 100,
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'Rolex.b1',
+  database: 'number', // Cambia esto al nombre de tu base de datos
+};
+const pool = mysql.createPool(connection);
+
+function runStoredProcedure(callback) {
+  // Conectar a la base de datos
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error:', err);
+      callback({ status: 'error' });
+      return;
+    }
+
+    // Ejecutar el procedimiento almacenado
+    connection.query('CALL suma()', (error, results) => {
+      if (error) {
+        console.error('Error:', error);
+        console.log('Se ha encontrado un error');
+        connection.release(); // Liberar la conexión en caso de error
+        callback({ status: 'error' });
+      } else {
+        console.log('Ok!');
+        callback({ status: 'ok', results: results[0] });
+      }
+
+      connection.release(); // Liberar la conexión en caso de éxito o error
+    });
+  });
+}
+
+// Ejecutar el procedimiento almacenado al iniciar el servidor
+runStoredProcedure((result) => {
+  if (result.status === 'ok') {
+    console.log('Procedimiento ejecutado correctamente');
+  } else {
+    console.log('Error al ejecutar el procedimiento');
+  }
+});
+
+app.get('/run-stored-procedure', (req, res) => {
+  runStoredProcedure((result) => {
+    if (result.status === 'ok') {
+      console.log('Procedimiento ejecutado correctamente en postman');
+    } else {
+      console.log('Error al ejecutar el procedimiento');
+    }
+
+    res.json(result);
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor en funcionamiento en el puerto ${port}`);
+});
